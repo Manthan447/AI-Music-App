6cb9509538a0181cf6e19e37e84be8d89f562db4
Manthan447 <76776298+Manthan447@users.noreply.github.com>
2022-02-25 17:12:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,35 +11,35 @@ scorerightwrist = 0;
 
 
 function preload() { song_1 = loadSound("music.mp3");
-song_2 = loadSound("music_2.mp3");
+song_2 = loadSound("music2.mp3");
 }
 
 
 function setup(){
-canvas = createCanvas(650 , 200 , 300 , 500);
+canvas = createCanvas(600,500);
 canvas.center();
 video = createCapture(VIDEO);
 video.hide();
-posenet = ml5.poseNet(video , modelloaded);
-posenet.on('pose' , gotPoses);
+poseNet = ml5.poseNet(video , modelLoaded);
+poseNet.on('pose',gotPoses);
 }
 
 function draw(){
     image(video,0,0,600,500);
-
+    song_1_status = song_1.isPlaying();
+    song_2_status = song_2.isPlaying();
     fill("#FF0000");
     stroke("#FF0000");
-    if(scorerightwrist >0.001){
+    if(scorerightwrist >0.2){
         circle(rightwrist_x , rightwrist_y, 20);
         song_2.stop();
         if(song_1_status == false){
             song_1.play();
             document.getElementById("song_name").innerHTML = "Playing Dj music";
         }
-    }
-        
-    }
-    if(scoreleftwrist > 0.002){   
+    }   
+    
+    if(scoreleftwrist > 0.2){   
         
     circle(leftwrist_x,leftwrist_y,20);
     song_1.stop();
@@ -48,9 +48,10 @@ function draw(){
         document.getElementById("song_name").innerHTML = "Playing Peter Pan music";
     }
     }
+}
 
 
-function modelloaded(){
+function modelLoaded(){
     console.log("Posenet is intialized");
 }
 
